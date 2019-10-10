34e00c989ff4d773738ec24df5d57c4345ba8daf
Yeswanth^k <epuri.yeshu@gmail.com>
2019-10-10 13:16:51 +0530

    Grade Book with some good structural example.

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -5,15 +5,37 @@ namespace GradeBook
 {
     internal class Book
     {
-        private List<double> Grade { get; set; }=new List<double>();
-        internal double GetAverageofGrades()
+        #region Private member
+        private string name { get; set; }
+        private static double HighGrade { get; set; } = double.MinValue;
+        private static double LowGrade { get; set; } = double.MaxValue;
+        private List<double> Grade { get; set; } = new List<double>();
+        
+        private double GetAverageofGrades()
         {
             var sum = 0.0;
             foreach (var item in Grade)
             {
+                HighGrade = Math.Max(item, HighGrade);
+                LowGrade = Math.Max(item, LowGrade);
                 sum += item;
             }
             return sum /= Grade.Count;
+        }
+        #endregion
+        
+        public Book(string name)
+        {
+            this.name = name;
+        }
+
+        internal void ShowStatics()
+        {
+            double result = GetAverageofGrades();
+            Console.WriteLine($"Average Grade is: {result}");
+            Console.WriteLine($"Highest Grade {Book.HighGrade}");
+            Console.WriteLine($"LowGrade is : {Book.LowGrade}");
+            Console.Read();
         }
 
         public void AddGrade(double gradeval)
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -6,15 +6,21 @@ namespace GradeBook
 {
     class Program
     {
+        /*
+        Static Key word - they are instance membors or methods of the class.
+        */
         static void Main(string[] args)
         {
-            Book book = new Book();
+            //Create a new object of Books and pass 
+            Book book = new Book("Yeswanth Grade Book");
             //var numbers = new []{ 10.2, 23.4, 24.3 };
             //var grade=new List<double>(){ 10.2, 23.4, 24.3 };
+            //Add some Grades
             book.AddGrade(56.1);
-            book.AddGrade(10.2);
-            double result = book.GetAverageofGrades();
-            Console.WriteLine(result);
+            book.AddGrade(90.2);
+            book.AddGrade(32.2);
+            //
+            book.ShowStatics();
         }
     }
 }
